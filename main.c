d802b33ca4cd0e81717ebed38a060055fa09a2da
Meaghan Boykin <meaghanb101@gmail.com>
2025-03-26 11:20:22 -0700

    piping made into functions
    multiple pipe logic

--- main.c
+++ main.c
@@ -10,93 +10,148 @@
 #include "parsetools.h"
 
 void syserror(const char *);
-void forkAndExec(char* words[], int pdfs[], int dupNum);
+
+int getProcessWords(char *pWords[], char *lWords[], int *lIndex);
+
+void forkAndExec(char *words[]);
+
+void forkAndExecWR(char *words[], int *pipes[], int numPipes);
+
+void forkAndExecRD(char *words[], int *pipes[], int numPipes);
+
+void forkAndExecRDWR(char *words[], int *pipes[], int numPipes);
+
+void closeAllPipes(int *pipes[], int numPipes);
 
 int main() {
 
     // Buffer for reading one line of input
     char line[MAX_LINE_CHARS];
     // holds separated words based on whitespace
-    char* line_words[MAX_LINE_WORDS + 1];
+    char *line_words[MAX_LINE_WORDS + 1];
+    // holds the pipes
+    // the thought is at most half the words can be commands
+    // ex w/ nine "words": ls | wc | wc | wc | tail (four pipes)
+    int *pipes[MAX_LINE_WORDS / 2 + 1];
+    int numPipes = 0;
 
     // Loop until user hits Ctrl-D (end of input)
     // or some other input error occurs
-    while( fgets(line, MAX_LINE_CHARS, stdin) ) {
-        int num_words = split_cmd_line(line, line_words);
-
-        /*for (int i=0; i < num_words; i++) {
-            printf("%s\n", line_words[i]);
-        }*/
+    while (fgets(line, MAX_LINE_CHARS, stdin)) {
+        split_cmd_line(line, line_words);
     }
 
-    char* p1Words[MAX_LINE_WORDS + 1];
     int lineIndex = 0;
-    int numP1Words;
-    while (strcmp(line_words[lineIndex], "|") != 0) {
-        p1Words[lineIndex] = line_words[lineIndex];
-        lineIndex++;
-    }
-    numP1Words = lineIndex;
-    p1Words[numP1Words] = NULL;
-    lineIndex++;
-
-    printf("P1Words:\n");
-    for (int i = 0; i < numP1Words; i++) {
-        printf("%s\n", p1Words[i]);
-    }
-
-    char* p2Words[MAX_LINE_WORDS + 1];
-    int numP2Words = 0;
-    while (line_words[lineIndex] != 0) {
-        p2Words[numP2Words] = line_words[lineIndex];
-        numP2Words++;
-        lineIndex++;
-    }
-    p2Words[numP2Words] = NULL;
-
-    printf("P2Words:\n");
-    for (int i = 0; i < numP2Words; i++) {
-        printf("%s\n", p2Words[i]);
-    }
-
-    int pdfs[2];
-    if (pipe(pdfs) == -1) {
-        syserror("ERROR: could not create a pipe.");
+    u_int8_t read = 0; // acting as boolean - is this process reading from a pipe?
+    while (1) {
+        char *pWords[MAX_LINE_WORDS + 1];
+        int nullRet = getProcessWords(pWords, line_words, &lineIndex);
+        if (nullRet == 0) { // exited from a NULL -> end of input, not writing to a pipe
+            if (read == 0) { // not reading from a pipe
+                forkAndExec(pWords);
+            }
+            else { // reading from a pipe
+                forkAndExecRD(pWords, pipes, numPipes);
+            }
+            break;
+        }
+        else if (read == 0) { // not reading from a pipe but writing to a pipe
+            int *pdfs = (int *) malloc(2 * sizeof(int));
+            pipe(pdfs);
+            pipes[numPipes] = pdfs;
+            numPipes++;
+            read = 1; // next process will be on the read end
+            forkAndExecWR(pWords, pipes, numPipes);
+        }
+        else { // reading and writing from a pipe
+            int *pdfs = (int *) malloc(2 * sizeof(int));
+            pipe(pdfs);
+            pipes[numPipes] = pdfs;
+            numPipes++;
+            // read is already 1
+            forkAndExecRDWR(pWords, pipes, numPipes);
+        }
     }
 
-    forkAndExec(p1Words, pdfs, 1);
-    forkAndExec(p2Words, pdfs, 0);
-
-    close (pdfs[0]);
-    close (pdfs[1]);
-    while (wait(NULL) != -1);
-    printf("Parent reaped child.\n");
+    closeAllPipes(pipes, numPipes);
+    while (wait(NULL) != -1); // reap children
 
     return 0;
 }
 
-void syserror(const char *s)
-{
+void syserror(const char *s) {
     extern int errno;
     fprintf(stderr, "%s\n", s);
     fprintf(stderr, " (%s)\n", strerror(errno));
     exit(1);
 }
 
-void forkAndExec(char* words[], int pdfs[], int dupNum) {
+void forkAndExec(char *words[]) {
+    int pid = fork();
+    if (pid != 0) {
+        return;
+    }
+    execvp(words[0], words);
+}
+
+void forkAndExecWR(char *words[], int *pipes[], int numPipes) {
     int pid = fork();
     if (pid != 0) {
         return;
     }
     // pdfs must be piped
-    if (pdfs != NULL) {
-        if (dupNum != 0 && dupNum != 1) {
-            syserror("Invalid dupNum.");
-        }
-        dup2(pdfs[dupNum], dupNum);
-        if (close(pdfs[0]) == -1 || close(pdfs[1]) == -1) {
-            syserror("Cannot close pdfs.");
-        }
-    }
+    dup2(pipes[numPipes - 1][1], 1);
+    closeAllPipes(pipes, numPipes);
     execvp(words[0], words);
+}
+
+void forkAndExecRD(char *words[], int *pipes[], int numPipes) {
+    int pid = fork();
+    if (pid != 0) {
+        return;
+    }
+    // pdfs must be piped
+    dup2(pipes[numPipes - 1][0], 0);
+    closeAllPipes(pipes, numPipes);
+    execvp(words[0], words);
+}
+
+void forkAndExecRDWR(char *words[], int *pipes[], int numPipes) {
+    int pid = fork();
+    if (pid != 0) {
+        return;
+    }
+    // pdfsRD/WR must be piped
+    dup2(pipes[numPipes - 2][0], 0);
+    dup2(pipes[numPipes - 1][1], 1);
+    closeAllPipes(pipes, numPipes);
+    execvp(words[0], words);
+}
+
+int getProcessWords(char *pWords[], char *lWords[], int *lIndex) {
+    // lIndex is a pointer bc it will be used further in main
+    // and its dereference value must be up-to-date for main
+    int pIndex = 0;
+    while (lWords[*lIndex] != NULL && strcmp(lWords[*lIndex], "|") != 0) {
+        pWords[pIndex] = lWords[*lIndex];
+        pIndex++;
+        (*lIndex)++;
+    }
+    pWords[pIndex] = NULL; // last word in array for execv() must be NULL
+
+    // return, indicating if hit a NULL or a pipe
+    if (lWords[*lIndex] == NULL) { // return from NULL
+        return 0;
+    } else { // return from pipe
+        (*lIndex)++;
+        return 1;
+    }
+}
+
+void closeAllPipes(int *pipes[], int numPipes) {
+    for (int i = 0; i < numPipes; i++) { // free pipes
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+        free(pipes[i]);
+    }
 }
